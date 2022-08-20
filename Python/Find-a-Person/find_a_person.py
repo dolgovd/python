bf85e78040f47909a06cdb324a1383bdaae9f540
Dmitry Dolgov <ddolgov@Luxoft-MacBook-Pro.local>
2022-08-20 10:17:24 +0200

    Added intructuion how to setup blackbird service on your localhost

---
+++ Python/Find-a-Person/find_a_person.py
@@ -0,0 +1,19 @@
+# Program allows find people in instagram, telegram, facebook and other popular social services
+# OSINT is used for gathering data
+# Existing project will be used: need to clone repository https://github.com/p1ngul1n0/blackbird
+
+# Create virtual end and activate it
+# python3 -m venv venv
+# source venv/bin/activate 
+
+# Install packages from requirements.txt
+# pip3 install -r requirements.txt
+
+# Get access to the service via command line
+# python blackbird.py -u elonmusk
+
+# Get access to the service via web interface
+# python blackbird.py --web
+
+# Deactivate venv
+# deactivate
