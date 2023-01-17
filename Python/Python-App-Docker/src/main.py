3591971991bce04e081752292911f19039d458ea
Dmitry Dolgov <d.dolgov@outlook.com>
2023-01-17 13:39:31 +0100

    Update Python-App-Docker

--- Python/Python-App-Docker/src/main.py
+++ Python/Python-App-Docker/src/main.py
@@ -1,15 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+import redis
 
 app = FastAPI()
 
+r = redis.Redis(host="redis", port=6379)
 
 @app.get("/")
 def read_root():
     return {"Hello mate"}
+    
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"number of hits": r.get("hits")}
